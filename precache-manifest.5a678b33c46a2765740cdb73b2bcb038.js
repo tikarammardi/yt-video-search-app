3d308ae364df6fd3989ce54b5ba01fbcfb8cb662
Tikaram Mardi <tikarammardi007@gmail.com>
2019-04-25 13:16:40 +0530

    Updates

---
+++ precache-manifest.5a678b33c46a2765740cdb73b2bcb038.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f65bd667d124482f2d08",
+    "url": "/yt-video-search-app/static/js/runtime~main.277d2c78.js"
+  },
+  {
+    "revision": "b01f9c919067e502c10d",
+    "url": "/yt-video-search-app/static/js/main.e71a64b9.chunk.js"
+  },
+  {
+    "revision": "ee788716df30e01b1a45",
+    "url": "/yt-video-search-app/static/js/2.a172d5a2.chunk.js"
+  },
+  {
+    "revision": "b01f9c919067e502c10d",
+    "url": "/yt-video-search-app/static/css/main.9617c50e.chunk.css"
+  },
+  {
+    "revision": "76718a2609f1a33349a4e049e6a0bdbf",
+    "url": "/yt-video-search-app/index.html"
+  }
+];
