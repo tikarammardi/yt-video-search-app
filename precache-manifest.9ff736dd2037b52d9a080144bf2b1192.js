db417c36e4a8f8545018f5fdd379493097a340fd
Tikaram Mardi <tikarammardi007@gmail.com>
2019-09-18 15:44:07 +0530

    Updates

---
+++ precache-manifest.9ff736dd2037b52d9a080144bf2b1192.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160b228218dd5d6bf538e6fa32da38b3",
+    "url": "/yt-video-search-app/index.html"
+  },
+  {
+    "revision": "6a6fe560a4e33a107b3e",
+    "url": "/yt-video-search-app/static/css/main.a48f476f.chunk.css"
+  },
+  {
+    "revision": "dd09e33f5fb31954e524",
+    "url": "/yt-video-search-app/static/js/2.cfb055af.chunk.js"
+  },
+  {
+    "revision": "6a6fe560a4e33a107b3e",
+    "url": "/yt-video-search-app/static/js/main.6e2885a9.chunk.js"
+  },
+  {
+    "revision": "0e34e622c1ea5f78e399",
+    "url": "/yt-video-search-app/static/js/runtime~main.76d88904.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yt-video-search-app/precache-manifest.620595ed79ba93e8a5ceb0c6d406ec26.js"
+  "/yt-video-search-app/precache-manifest.9ff736dd2037b52d9a080144bf2b1192.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/yt-video-search-app/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yt-video-search-app/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
